4e02927e5526584c71c2ffba5864b0aa48ac6551
Sunrise <79020755+SunriseWasTaken@users.noreply.github.com>
2024-08-16 12:18:53 +0100

    main

---
+++ main.js
@@ -0,0 +1,4 @@
+// Simple script to toggle visibility of a sidebar (if applicable)
+document.querySelector('.sidebar-toggle').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('active');
+});
